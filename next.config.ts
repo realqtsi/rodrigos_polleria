f8c9f382bb9efb3c3ac30280f5683fd718fa0af4
iblamejssee <catzelmarket@gmail.com>
2026-04-11 14:03:07 -0500

    Agregar instalador.bat, env.local y configuracion para migracion laptop

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  srcDir: "./src",
   reactCompiler: true,
   images: {
     remotePatterns: [],
